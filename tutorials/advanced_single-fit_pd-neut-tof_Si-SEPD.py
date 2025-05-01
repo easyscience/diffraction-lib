d7204d624f8e446d82f5765396b8e801e00b5684
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-05-01 15:07:55 +0200

    Rename tutorial files

--- tutorials/advanced_single-fit_pd-neut-tof_Si-SEPD.py
+++ tutorials/advanced_single-fit_pd-neut-tof_Si-SEPD.py
@@ -1,10 +1,8 @@
 # %% [markdown]
-# # Joint Fit (Advanced Usage)
+# # Standard diffraction: Si
 #
-# This example demonstrates a more flexible and advanced usage of the
-# EasyDiffraction library by explicitly creating and configuring some objects.
-# It is more suitable for users comfortable with Python programming and those
-# interested in custom workflows.
+# Standard diffraction analysis of Si after the powder neutron time-of-flight
+# diffraction measurement from SEPD at Argone.
 
 # %% [markdown]
 # ## Import EasyDiffraction
@@ -25,167 +23,88 @@ from easydiffraction import (
 # ### Create sample model object
 
 # %%
-model = SampleModel('pbso4')
+model = SampleModel('si')
 
 # %% [markdown]
 # ### Define space group
 
 # %%
-model.space_group.name_h_m = 'P n m a'
+model.space_group.name_h_m = 'F d -3 m'
+model.space_group.it_coordinate_system_code = 2
 
 # %% [markdown]
 # ### Define unit cell
 
 # %%
-model.cell.length_a = 8.47
-model.cell.length_b = 5.39
-model.cell.length_c = 6.95
+model.cell.length_a = 5.431
 
 # %% [markdown]
 # ### Define atom sites
 
 # %%
-model.atom_sites.add('Pb', 'Pb', 0.1876, 0.25, 0.167, b_iso=1.37)
-model.atom_sites.add('S', 'S', 0.0654, 0.25, 0.684, b_iso=0.3777)
-model.atom_sites.add('O1', 'O', 0.9082, 0.25, 0.5954, b_iso=1.9764)
-model.atom_sites.add('O2', 'O', 0.1935, 0.25, 0.5432, b_iso=1.4456)
-model.atom_sites.add('O3', 'O', 0.0811, 0.0272, 0.8086, b_iso=1.2822)
-
+model.atom_sites.add('Si', 'Si', 0.125, 0.125, 0.125, b_iso=0.5)
 
 # %% [markdown]
-# ## Define Experiments
+# ## Define Experiment
 #
 # This section teaches how to add experiments, configure their parameters, and
 # link to them the sample models defined in the previous step.
 #
-# ### Experiment 1: npd
-#
-# #### Download measured data
+# ### Download measured data
 
 # %%
-download_from_repository('d1a_pbso4.dat',
+download_from_repository('sepd_si.xye',
                          branch='docs',
                          destination='data')
 
 # %% [markdown]
-# #### Create experiment object
+# ### Create experiment object
 
 # %%
-expt1 = Experiment('npd',
-                   radiation_probe='neutron',
-                   data_path='data/d1a_pbso4.dat')
+expt = Experiment('sepd',
+                  beam_mode='time-of-flight',
+                  data_path='data/sepd_si.xye')
 
 # %% [markdown]
-# #### Define instrument
+# ### Define instrument
 
 # %%
-expt1.instrument.setup_wavelength = 1.91
-expt1.instrument.calib_twotheta_offset = -0.1406
+expt.instrument.setup_twotheta_bank = 144.845
+expt.instrument.calib_d_to_tof_offset = 0.0
+expt.instrument.calib_d_to_tof_linear = 7476.91
+expt.instrument.calib_d_to_tof_quad = -1.54
 
 # %% [markdown]
-# #### Define peak profile
+# ### Define peak profile
 
 # %%
-expt1.peak.broad_gauss_u = 0.139
-expt1.peak.broad_gauss_v = -0.412
-expt1.peak.broad_gauss_w = 0.386
-expt1.peak.broad_lorentz_x = 0
-expt1.peak.broad_lorentz_y = 0.088
+expt.peak_profile_type = 'pseudo-voigt * ikeda-carpenter'
+expt.peak.broad_gauss_sigma_0 = 3.0
+expt.peak.broad_gauss_sigma_1 = 40.0
+expt.peak.broad_gauss_sigma_2 = 2.0
+expt.peak.broad_mix_beta_0 = 0.04221
+expt.peak.broad_mix_beta_1 = 0.00946
 
 # %% [markdown]
-# #### Define background
-
-# %% [markdown]
-# Select desired background type
+# ### Define peak asymmetry
 
 # %%
-expt1.background_type = 'line-segment'
+expt.peak.asym_alpha_0 = 0.0
+expt.peak.asym_alpha_1 = 0.5971
 
 # %% [markdown]
-# Add background points
+# ### Define background
 
 # %%
-for x, y in [
-    (11.0, 206.1624),
-    (15.0, 194.75),
-    (20.0, 194.505),
-    (30.0, 188.4375),
-    (50.0, 207.7633),
-    (70.0, 201.7002),
-    (120.0, 244.4525),
-    (153.0, 226.0595),
-]:
-    expt1.background.add(x, y)
+expt.background_type = 'line-segment'
+for x in range(0, 35000, 5000):
+    expt.background.add(x=x, y=200)
 
 # %% [markdown]
-# #### Define linked phases
+# ### Select linked phase
 
 # %%
-expt1.linked_phases.add('pbso4', scale=1.5)
-
-# %% [markdown]
-# ### Experiment 2: xrd
-#
-# #### Download measured data
-
-# %%
-download_from_repository('lab_pbso4.dat',
-                         branch='docs',
-                         destination='data')
-
-# %% [markdown]
-# #### Create experiment object
-
-# %%
-expt2 = Experiment('xrd',
-                   radiation_probe='xray',
-                   data_path='data/lab_pbso4.dat')
-
-# %% [markdown]
-# #### Define instrument
-
-# %%
-expt2.instrument.setup_wavelength = 1.540567
-expt2.instrument.calib_twotheta_offset = -0.05181
-
-# %% [markdown]
-# #### Define peak profile
-
-# %%
-expt2.peak.broad_gauss_u = 0.304138
-expt2.peak.broad_gauss_v = -0.112622
-expt2.peak.broad_gauss_w = 0.021272
-expt2.peak.broad_lorentz_x = 0
-expt2.peak.broad_lorentz_y = 0.057691
-
-# %% [markdown]
-# #### Define background
-
-# %% [markdown]
-# Select desired background type
-
-# %%
-expt2.background_type = 'chebyshev polynomial'
-
-# %% [markdown]
-# Add background points
-
-# %%
-for x, y in [
-    (0, 119.195),
-    (1, 6.221),
-    (2, -45.725),
-    (3, 8.119),
-    (4, 54.552),
-    (5, -20.661),
-]:
-    expt2.background.add(x, y)
-
-# %% [markdown]
-# #### Define linked phases
-
-# %%
-expt2.linked_phases.add('pbso4', scale=0.001)
+expt.linked_phases.add('si', scale=10.0)
 
 # %% [markdown]
 # ## Define Project
@@ -205,11 +124,10 @@ project = Project()
 project.sample_models.add(model)
 
 # %% [markdown]
-# ### Add experiments
+# ### Add experiment
 
 # %%
-project.experiments.add(expt1)
-project.experiments.add(expt2)
+project.experiments.add(expt)
 
 # %% [markdown]
 # ## Analysis
@@ -223,68 +141,150 @@ project.experiments.add(expt2)
 project.analysis.current_calculator = 'cryspy'
 
 # %% [markdown]
-# ### Set fit mode
-
-# %%
-project.analysis.fit_mode = 'joint'
-
-# %% [markdown]
 # ### Set fitting engine
 
 # %%
 project.analysis.current_minimizer = 'lmfit (leastsq)'
 
 # %% [markdown]
-# ### Set fitting parameters
+# ### Show measured vs calculated
+
+# %%
+project.plot_meas_vs_calc(expt_name='sepd',
+                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='sepd',
+                          x_min=23200, x_max=23700,
+                          show_residual=True)
+
+# %% [markdown]
+# ### Fitting Step 1/5
 #
-# Set sample model parameters to be fitted
+# Set parameters to be fitted
 
 # %%
 model.cell.length_a.free = True
-model.cell.length_b.free = True
-model.cell.length_c.free = True
+
+expt.linked_phases['si'].scale.free = True
+expt.instrument.calib_d_to_tof_offset.free = True
 
 # %% [markdown]
-# Set experimental parameters to be fitted
+# Show free parameters after selection
 
 # %%
-expt1.linked_phases['pbso4'].scale.free = True
-
-expt1.instrument.calib_twotheta_offset.free = True
-
-expt1.peak.broad_gauss_u.free = True
-expt1.peak.broad_gauss_v.free = True
-expt1.peak.broad_gauss_w.free = True
-expt1.peak.broad_lorentz_y.free = True
-
-# %%
-expt2.linked_phases['pbso4'].scale.free = True
-
-expt2.instrument.calib_twotheta_offset.free = True
-
-expt2.peak.broad_gauss_u.free = True
-expt2.peak.broad_gauss_v.free = True
-expt2.peak.broad_gauss_w.free = True
-expt2.peak.broad_lorentz_y.free = True
-
-for term in expt2.background:
-    term.coef.free = True
+project.analysis.show_free_params()
 
 # %% [markdown]
-# ### Fitting step
+# #### Start fitting
 
 # %%
 project.analysis.fit()
 
 # %% [markdown]
-# ### Show fitting results
+# #### Show fitting results
 
 # %%
-project.plot_meas_vs_calc(expt_name='npd',
-                          x_min=35.5, x_max=38.3,
+project.plot_meas_vs_calc(expt_name='sepd',
+                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='sepd',
+                          x_min=23200, x_max=23700,
                           show_residual=True)
 
+# %% [markdown]
+# ### Fitting Step 2/5
+#
+# Set parameters to be fitted
+
 # %%
-project.plot_meas_vs_calc(expt_name='xrd',
-                          x_min=29.0, x_max=30.4,
+for point in expt.background:
+    point.y.free = True
+
+# %% [markdown]
+# Show free parameters after selection
+
+# %%
+project.analysis.show_free_params()
+
+# %% [markdown]
+# #### Start fitting
+
+# %%
+project.analysis.fit()
+
+# %% [markdown]
+# #### Show fitting results
+
+# %%
+project.plot_meas_vs_calc(expt_name='sepd',
+                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='sepd',
+                          x_min=23200, x_max=23700,
+                          show_residual=True)
+
+# %% [markdown]
+# ### Fitting Step 3/5
+#
+# Fix background points
+
+# %%
+for point in expt.background:
+    point.y.free = False
+
+# %% [markdown]
+# Set parameters to be fitted
+
+# %%
+expt.peak.broad_gauss_sigma_0.free = True
+expt.peak.broad_gauss_sigma_1.free = True
+expt.peak.broad_gauss_sigma_2.free = True
+
+# %% [markdown]
+# Show free parameters after selection
+
+# %%
+project.analysis.show_free_params()
+
+# %% [markdown]
+# #### Start fitting
+
+# %%
+project.analysis.fit()
+
+# %% [markdown]
+# #### Show fitting results
+
+# %%
+project.plot_meas_vs_calc(expt_name='sepd',
+                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='sepd',
+                          x_min=23200, x_max=23700,
+                          show_residual=True)
+
+# %% [markdown]
+# ### Fitting Step 4/5
+#
+# Set parameters to be fitted
+
+# %%
+model.atom_sites['Si'].b_iso.free = True
+
+# %% [markdown]
+# Show free parameters after selection
+
+# %%
+project.analysis.show_free_params()
+
+# %% [markdown]
+# #### Start fitting
+
+# %%
+project.analysis.fit()
+
+# %% [markdown]
+# #### Show fitting results
+
+# %%
+project.plot_meas_vs_calc(expt_name='sepd',
+                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='sepd',
+                          x_min=23200, x_max=23700,
                           show_residual=True)
