f84bda4a4dabb2393d7c05d2b1bda8bc61ce6304
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-05-05 20:28:05 +0200

    Updates NCAF tutorial

--- tutorials/cryst-struct_pd-neut-tof_NCAF-WISH.py
+++ tutorials/cryst-struct_pd-neut-tof_NCAF-WISH.py
@@ -5,7 +5,7 @@
 # structure using time-of-flight neutron powder diffraction data from WISH at
 # ISIS.
 #
-# Temporarily, only a single dataset from detector banks 5 & 6 is used here.
+# Two datasets from detector banks 5_6 and 4_7 are used for joint fitting.
 
 # %% [markdown]
 # ## Import Library
@@ -45,12 +45,12 @@ model.cell.length_a = 10.250256
 # ### Set Atom Sites
 
 # %%
-model.atom_sites.add('Ca', 'Ca', 0.4661, 0.0, 0.25, wyckoff_letter="b", b_iso=0.9)
-model.atom_sites.add('Al', 'Al', 0.25171, 0.25171, 0.25171, wyckoff_letter="a", b_iso=0.66)
-model.atom_sites.add('Na', 'Na', 0.08481, 0.08481, 0.08481, wyckoff_letter="a", b_iso=1.9)
-model.atom_sites.add('F1', 'F', 0.1375, 0.3053, 0.1195, wyckoff_letter="c", b_iso=0.9)
-model.atom_sites.add('F2', 'F', 0.3626, 0.3634, 0.1867, wyckoff_letter="c", b_iso=1.28)
-model.atom_sites.add('F3', 'F', 0.4612, 0.4612, 0.4612, wyckoff_letter="a", b_iso=0.79)
+model.atom_sites.add('Ca', 'Ca', 0.4665, 0.0, 0.25, wyckoff_letter="b", b_iso=0.92)
+model.atom_sites.add('Al', 'Al', 0.2520, 0.2520, 0.2520, wyckoff_letter="a", b_iso=0.73)
+model.atom_sites.add('Na', 'Na', 0.0852, 0.0852, 0.0852, wyckoff_letter="a", b_iso=2.08)
+model.atom_sites.add('F1', 'F', 0.1376, 0.3055, 0.1196, wyckoff_letter="c", b_iso=0.90)
+model.atom_sites.add('F2', 'F', 0.3623, 0.3631, 0.1870, wyckoff_letter="c", b_iso=1.37)
+model.atom_sites.add('F3', 'F', 0.4611, 0.4611, 0.4611, wyckoff_letter="a", b_iso=0.88)
 
 # %% [markdown]
 # ## Define Experiment
@@ -61,50 +61,61 @@ model.atom_sites.add('F3', 'F', 0.4612, 0.4612, 0.4612, wyckoff_letter="a", b_is
 # ### Download Measured Data
 
 # %%
-download_from_repository('wish_ncaf.xye',
-                         branch='docs',
-                         destination='data')
+download_from_repository('wish_ncaf_5_6.xye', branch='docs', destination='data')
+
+# %%
+download_from_repository('wish_ncaf_4_7.xye', branch='docs', destination='data')
 
 # %% [markdown]
 # ### Create Experiment
 
 # %%
-expt = Experiment('wish',
-                  beam_mode='time-of-flight',
-                  data_path='data/wish_ncaf.xye')
+expt56 = Experiment('wish_5_6', beam_mode='time-of-flight', data_path='data/wish_ncaf_5_6.xye')
+
+# %%
+expt47 = Experiment('wish_4_7', beam_mode='time-of-flight', data_path='data/wish_ncaf_4_7.xye')
 
 # %% [markdown]
 # ### Set Instrument
 
 # %%
-expt.instrument.setup_twotheta_bank = 152.827
-expt.instrument.calib_d_to_tof_offset = 0.0
-expt.instrument.calib_d_to_tof_linear = 20770
-expt.instrument.calib_d_to_tof_quad = -1.08308
+expt56.instrument.setup_twotheta_bank = 152.827
+expt56.instrument.calib_d_to_tof_offset = -13.0
+expt56.instrument.calib_d_to_tof_linear = 20752.1
+expt56.instrument.calib_d_to_tof_quad = -1.08308
+
+# %%
+expt47.instrument.setup_twotheta_bank = 121.660
+expt47.instrument.calib_d_to_tof_offset = -15.0
+expt47.instrument.calib_d_to_tof_linear = 18640.7
+expt47.instrument.calib_d_to_tof_quad = -0.47488
 
 # %% [markdown]
 # ### Set Peak Profile
 
 # %%
-expt.peak_profile_type = 'pseudo-voigt * ikeda-carpenter'
-expt.peak.broad_gauss_sigma_0 = 0.0
-expt.peak.broad_gauss_sigma_1 = 0.0
-expt.peak.broad_gauss_sigma_2 = 5.0
-expt.peak.broad_mix_beta_0 = 0.01
-expt.peak.broad_mix_beta_1 = 0.01
-
-# %% [markdown]
-# ### Set Peak Asymmetry
+expt56.peak.broad_gauss_sigma_0 = 0.0
+expt56.peak.broad_gauss_sigma_1 = 0.0
+expt56.peak.broad_gauss_sigma_2 = 15.0
+expt56.peak.broad_mix_beta_0 = 0.01
+expt56.peak.broad_mix_beta_1 = 0.01
+expt56.peak.asym_alpha_0 = -0.01
+expt56.peak.asym_alpha_1 = 0.10
 
 # %%
-expt.peak.asym_alpha_0 = 0.0
-expt.peak.asym_alpha_1 = 0.1
+expt47.peak.broad_gauss_sigma_0 = 0.0
+expt47.peak.broad_gauss_sigma_1 = 30.0
+expt47.peak.broad_gauss_sigma_2 = 20.0
+expt47.peak.broad_mix_beta_0 = 0.01
+expt47.peak.broad_mix_beta_1 = 0.01
+expt47.peak.asym_alpha_0 = -0.01
+expt47.peak.asym_alpha_1 = 0.10
 
 # %% [markdown]
 # ### Set Background
 
 # %%
-expt.background_type = 'line-segment'
+expt56.background_type = 'line-segment'
 for x, y in [
     (9162, 465),
     (11136, 593),
@@ -135,13 +146,49 @@ for x, y in [
     (91958, 268),
     (102712, 262)
 ]:
-    expt.background.add(x, y)
+    expt56.background.add(x, y)
+
+# %%
+expt47.background_type = 'line-segment'
+for x, y in [
+    (9090, 488),
+    (10672, 566),
+    (12287, 494),
+    (14037, 559),
+    (15451, 529),
+    (16764, 445),
+    (18076, 460),
+    (19456, 413),
+    (20466, 511),
+    (21880, 396),
+    (23798, 391),
+    (25447, 385),
+    (28073, 349),
+    (30058, 332),
+    (32583, 309),
+    (34804, 355),
+    (37160, 318),
+    (40324, 290),
+    (46895, 260),
+    (50631, 256),
+    (54602, 246),
+    (58439, 264),
+    (66520, 250),
+    (75002, 258),
+    (83649, 257),
+    (92770, 255),
+    (101524, 260)
+]:
+    expt47.background.add(x, y)
 
 # %% [markdown]
 # ### Set Linked Phases
 
 # %%
-expt.linked_phases.add('ncaf', scale=0.5)
+expt56.linked_phases.add('ncaf', scale=1.0)
+
+# %%
+expt47.linked_phases.add('ncaf', scale=2.0)
 
 # %% [markdown]
 # ## Define Project
@@ -170,7 +217,8 @@ project.sample_models.add(model)
 # ### Add Experiment
 
 # %%
-project.experiments.add(expt)
+project.experiments.add(expt56)
+project.experiments.add(expt47)
 
 # %% [markdown]
 # ## Analysis
@@ -190,93 +238,24 @@ project.analysis.current_calculator = 'cryspy'
 project.analysis.current_minimizer = 'lmfit (leastsq)'
 
 # %% [markdown]
+# ### Set Fit Mode
+
+# %%
+project.analysis.fit_mode = 'joint'
+
+# %% [markdown]
 # ### Plot Measured vs Calculated
 
 # %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='wish_5_6', show_residual=True)
 
 # %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          x_min=37000, x_max=40000,
-                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='wish_4_7', show_residual=True)
 
 # %% [markdown]
-# ### Perform Fit 1/5
+# ### Perform Fit
 #
 # Set parameters to be refined.
-
-# %%
-model.cell.length_a.free = True
-
-expt.instrument.calib_d_to_tof_offset.free = True
-expt.linked_phases['ncaf'].scale.free = True
-
-# %% [markdown]
-# Show free parameters after selection.
-
-# %%
-project.analysis.show_free_params()
-
-# %% [markdown]
-# #### Run Fit
-
-# %%
-project.analysis.fit()
-
-# %% [markdown]
-# ### Plot Measured vs Calculated
-
-# %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          show_residual=True)
-
-# %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          x_min=37000, x_max=40000,
-                          show_residual=True)
-
-# %% [markdown]
-# ### Perform Fit 2/5
-#
-# Set more parameters to be refined.
-
-# %%
-expt.peak.broad_gauss_sigma_2.free = True
-expt.peak.broad_mix_beta_0.free = True
-expt.peak.broad_mix_beta_1.free = True
-expt.peak.asym_alpha_0.free = True
-expt.peak.asym_alpha_1.free = True
-
-# %% [markdown]
-# Show free parameters after selection.
-
-# %%
-project.analysis.show_free_params()
-
-# %% [markdown]
-# #### Run Fit
-
-# %%
-project.analysis.fit()
-
-# %% [markdown]
-# #### Plot Measured vs Calculated
-
-# %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          show_residual=True)
-
-# %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          x_min=37000, x_max=40000,
-                          show_residual=True)
-
-
-# %% [markdown]
-# ### Perform Fit 3/5
-#
-# Set more parameters to be refined.
 
 # %%
 model.atom_sites['Ca'].fract_x.free = True
@@ -290,38 +269,6 @@ model.atom_sites['F2'].fract_y.free = True
 model.atom_sites['F2'].fract_z.free = True
 model.atom_sites['F3'].fract_x.free = True
 
-# %% [markdown]
-# Show free parameters after selection.
-
-# %%
-project.analysis.show_free_params()
-
-# %% [markdown]
-# #### Run Fit
-
-# %%
-project.analysis.fit()
-
-# %% [markdown]
-# #### Plot Measured vs Calculated
-
-# %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          show_residual=True)
-
-# %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          x_min=37000, x_max=40000,
-                          show_residual=True)
-
-# %% [markdown]
-# ### Perform Fit 4/5
-#
-# Set more parameters to be refined.
-
-# %%
-expt.instrument.calib_d_to_tof_linear.free = True
-
 model.atom_sites['Ca'].b_iso.free = True
 model.atom_sites['Al'].b_iso.free = True
 model.atom_sites['Na'].b_iso.free = True
@@ -329,6 +276,26 @@ model.atom_sites['F1'].b_iso.free = True
 model.atom_sites['F2'].b_iso.free = True
 model.atom_sites['F3'].b_iso.free = True
 
+# %%
+expt56.linked_phases['ncaf'].scale.free = True
+expt56.instrument.calib_d_to_tof_offset.free = True
+expt56.instrument.calib_d_to_tof_linear.free = True
+expt56.peak.broad_gauss_sigma_2.free = True
+expt56.peak.broad_mix_beta_0.free = True
+expt56.peak.broad_mix_beta_1.free = True
+expt56.peak.asym_alpha_0.free = True
+expt56.peak.asym_alpha_1.free = True
+
+expt47.linked_phases['ncaf'].scale.free = True
+expt47.instrument.calib_d_to_tof_linear.free = True
+expt47.instrument.calib_d_to_tof_offset.free = True
+expt47.peak.broad_gauss_sigma_1.free = True
+expt47.peak.broad_gauss_sigma_2.free = True
+expt47.peak.broad_mix_beta_0.free = True
+expt47.peak.broad_mix_beta_1.free = True
+expt47.peak.asym_alpha_0.free = True
+expt47.peak.asym_alpha_1.free = True
+
 # %% [markdown]
 # Show free parameters after selection.
 
@@ -345,13 +312,10 @@ project.analysis.fit()
 # #### Plot Measured vs Calculated
 
 # %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='wish_5_6', show_residual=True)
 
 # %%
-project.plot_meas_vs_calc(expt_name='wish',
-                          x_min=37000, x_max=40000,
-                          show_residual=True)
+project.plot_meas_vs_calc(expt_name='wish_4_7', show_residual=True)
 
 # %% [markdown]
 # ## Summary
