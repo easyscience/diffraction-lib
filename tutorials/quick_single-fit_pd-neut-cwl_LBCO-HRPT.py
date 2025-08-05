03df207da0856eab85eda76221c7666cf85b0106
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-08-05 16:51:36 +0200

    Removes CIF display call

--- tutorials/quick_single-fit_pd-neut-cwl_LBCO-HRPT.py
+++ tutorials/quick_single-fit_pd-neut-cwl_LBCO-HRPT.py
@@ -82,10 +82,6 @@ project.experiments['hrpt'].excluded_regions.add(minimum=0, maximum=5)
 project.experiments['hrpt'].excluded_regions.add(minimum=165, maximum=180)
 
 # %%
-project.experiments['hrpt'].show_as_cif()
-
-
-# %%
 project.experiments['hrpt'].linked_phases.add(id='lbco', scale=10.0)
 
 # %% [markdown]
