c6579a7dc7f7e42d7782222d03d3fb28a3f97964
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-08-18 08:49:04 +0200

    Merges is_pycharm into is_notebook and adds is_colab

--- src/easydiffraction/utils/utils.py
+++ src/easydiffraction/utils/utils.py
@@ -79,14 +79,19 @@ def is_notebook() -> bool:
     if IPython is None:
         return False
 
+    if is_pycharm(): # Running inside PyCharm
+        return True
+    elif is_colab(): # Running inside Google Colab
+        return True
+
     try:
         shell = get_ipython().__class__.__name__  # noqa: F821
-        if shell == 'ZMQInteractiveShell':
-            return True  # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (unlikely)
+        if shell == 'ZMQInteractiveShell':  # Jupyter notebook or qtconsole
+            return True
+        elif shell == 'TerminalInteractiveShell':   # Terminal running IPython
+            return False
+        else:  # Other type (unlikely)
+            return False
     except NameError:
         return False  # Probably standard Python interpreter
 
@@ -99,6 +104,20 @@ def is_pycharm() -> bool:
         bool: True if running inside PyCharm, False otherwise.
     """
     return os.environ.get('PYCHARM_HOSTED') == '1'
+
+
+def is_colab() -> bool:
+    """
+    Determines if the current environment is Google Colab.
+
+    Returns:
+        bool: True if running in Google Colab PyCharm, False otherwise.
+    """
+    try:
+        import google.colab
+        return True
+    except ImportError:
+        return False
 
 
 def is_github_ci() -> bool:
