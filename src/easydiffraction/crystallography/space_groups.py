af82ab9512d0fee913ad05c231ab925877689818
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-09-09 23:08:19 +0200

    Add workflow for code complexity analysis

--- src/easydiffraction/crystallography/space_groups.py
+++ src/easydiffraction/crystallography/space_groups.py
@@ -1,10 +1,12 @@
-import pickle
 import gzip
+import pickle
 from pathlib import Path
 
+
 def _load():
-    path = Path(__file__).with_name("space_groups.pkl.gz")
-    with gzip.open(path, "rb") as f:
-        return pickle.load(f)
+    path = Path(__file__).with_name('space_groups.pkl.gz')
+    with gzip.open(path, 'rb') as f:
+        return pickle.load(f)  # noqa: S301 safe: internal trusted data
+
 
 SPACE_GROUP_LOOKUP_DICT = _load()
