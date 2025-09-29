8dc9c013fd2937e093e3d9e5d09fdbade1391f03
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-09-29 22:09:20 +0200

    Add SPDX license headers to source files

--- src/easydiffraction/crystallography/space_groups.py
+++ src/easydiffraction/crystallography/space_groups.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021-2025 EasyDiffraction contributors <https://github.com/easyscience/diffraction>
+# SPDX-License-Identifier: BSD-3-Clause
+
 import gzip
 import pickle  # noqa: S403 - trusted internal pickle file (package data only)
 from pathlib import Path
--- src/easydiffraction/utils/logging.py
+++ src/easydiffraction/utils/logging.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021-2025 EasyDiffraction contributors <https://github.com/easyscience/diffraction>
+# SPDX-License-Identifier: BSD-3-Clause
+
 from __future__ import annotations
 
 import logging
