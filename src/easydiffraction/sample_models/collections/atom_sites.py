74d32bd193ee6b31e1aac77282a5f2a98f647533
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-10-08 17:42:37 +0200

    Adds missing newline in atom_sites.py

--- src/easydiffraction/sample_models/collections/atom_sites.py
+++ src/easydiffraction/sample_models/collections/atom_sites.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2021-2025 EasyDiffraction contributors <https://github.com/easyscience/diffraction>
 # SPDX-License-Identifier: BSD-3-Clause
+
 from cryspy.A_functions_base.database import DATABASE
 
 from easydiffraction.core.categories import CategoryCollection
