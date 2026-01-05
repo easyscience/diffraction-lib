b9d031693a89ad241e5d831e4d317a22bf55c143
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-01-05 12:10:16 +0100

    Updates SPDX headers to 2026

--- tests/unit/easydiffraction/summary/test_summary.py
+++ tests/unit/easydiffraction/summary/test_summary.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2021-2025 EasyDiffraction contributors <https://github.com/easyscience/diffraction>
+# SPDX-FileCopyrightText: 2021-2026 EasyDiffraction contributors <https://github.com/easyscience/diffraction>
 # SPDX-License-Identifier: BSD-3-Clause
 
 def test_summary_as_cif_returns_placeholder_string():
--- tests/unit/easydiffraction/utils/test_theme_detect.py
+++ tests/unit/easydiffraction/utils/test_theme_detect.py
@@ -1,5 +1,6 @@
-# SPDX-FileCopyrightText: 2021-2025 EasyDiffraction contributors
+# SPDX-FileCopyrightText: 2021-2026 EasyDiffraction contributors <https://github.com/easyscience/diffraction>
 # SPDX-License-Identifier: BSD-3-Clause
+
 """Unit tests for theme detection module."""
 
 from __future__ import annotations
