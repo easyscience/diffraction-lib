dd24606929b434ab91ebef8feb2ba3dbf9990c7e
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-01-28 16:28:25 +0100

    Implement the linked crystal category

--- src/easydiffraction/experiments/categories/linked_crystal.py
+++ src/easydiffraction/experiments/categories/linked_crystal.py
@@ -1,8 +1,7 @@
 # SPDX-FileCopyrightText: 2021-2026 EasyDiffraction contributors <https://github.com/easyscience/diffraction>
 # SPDX-License-Identifier: BSD-3-Clause
-"""Linked phases allow combining phases with scale factors."""
+"""Linked crystal category for single crystal experiments."""
 
-from easydiffraction.core.category import CategoryCollection
 from easydiffraction.core.category import CategoryItem
 from easydiffraction.core.parameters import Parameter
 from easydiffraction.core.parameters import StringDescriptor
@@ -13,20 +12,20 @@ from easydiffraction.core.validation import RegexValidator
 from easydiffraction.io.cif.handler import CifHandler
 
 
-class LinkedPhase(CategoryItem):
-    """Link to a phase by id with a scale factor."""
+class LinkedCrystal(CategoryItem):
+    """Link to a crystal by id with a scale factor."""
 
     def __init__(
         self,
         *,
         id=None,  # TODO: need new name instead of id
         scale=None,
-    ):
+    ) -> None:
         super().__init__()
 
         self._id = StringDescriptor(
             name='id',
-            description='Identifier of the linked phase.',
+            description='Identifier of the linked crystal.',
             value_spec=AttributeSpec(
                 value=id,
                 type_=DataTypes.STRING,
@@ -35,13 +34,13 @@ class LinkedPhase(CategoryItem):
             ),
             cif_handler=CifHandler(
                 names=[
-                    '_pd_phase_block.id',
+                    '_sc_crystal_block.id',
                 ]
             ),
         )
         self._scale = Parameter(
             name='scale',
-            description='Scale factor of the linked phase.',
+            description='Scale factor of the linked crystal.',
             value_spec=AttributeSpec(
                 value=scale,
                 type_=DataTypes.NUMERIC,
@@ -50,22 +49,21 @@ class LinkedPhase(CategoryItem):
             ),
             cif_handler=CifHandler(
                 names=[
-                    '_pd_phase_block.scale',
+                    '_sc_crystal_block.scale',
                 ]
             ),
         )
 
-        self._identity.category_code = 'linked_phases'
-        self._identity.category_entry_name = lambda: str(self.id.value)
+        self._identity.category_code = 'linked_crystal'
 
     @property
     def id(self) -> StringDescriptor:
-        """Identifier of the linked phase."""
+        """Identifier of the linked crystal."""
         return self._id
 
     @id.setter
     def id(self, value: str):
-        """Set the linked phase identifier."""
+        """Set the linked crystal identifier."""
         self._id.value = value
 
     @property
@@ -77,11 +75,3 @@ class LinkedPhase(CategoryItem):
     def scale(self, value: float):
         """Set scale factor value."""
         self._scale.value = value
-
-
-class LinkedPhases(CategoryCollection):
-    """Collection of LinkedPhase instances."""
-
-    def __init__(self):
-        """Create an empty collection of linked phases."""
-        super().__init__(item_type=LinkedPhase)
