ca8d73673dfd8e1685f118405b574e99e4e9b3c7
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-04-03 13:13:35 +0200

    Add new integration test

--- tests/integration/fitting/test_powder-diffraction_constant-wavelength.py
+++ tests/integration/fitting/test_powder-diffraction_constant-wavelength.py
@@ -479,6 +479,56 @@ def test_fit_neutron_pd_cwl_hs() -> None:
     )
 
 
+def test_single_fit_neutron_pd_cwl_lbco_with_constraints_from_project() -> None:
+    import easydiffraction as ed
+
+    # Create a project from CIF files
+    project = ed.Project()
+    project.structures.add_from_cif_path(ed.download_data(id=1, destination='data'))
+    project.experiments.add_from_cif_path(ed.download_data(id=2, destination='data'))
+
+    # Set constraints
+    project.analysis.aliases.create(
+        label='biso_La',
+        param=project.structures['lbco'].atom_sites['La'].b_iso,
+    )
+    project.analysis.aliases.create(
+        label='biso_Ba',
+        param=project.structures['lbco'].atom_sites['Ba'].b_iso,
+    )
+
+    project.analysis.aliases.create(
+        label='occ_La',
+        param=project.structures['lbco'].atom_sites['La'].occupancy,
+    )
+    project.analysis.aliases.create(
+        label='occ_Ba',
+        param=project.structures['lbco'].atom_sites['Ba'].occupancy,
+    )
+
+    project.analysis.constraints.create(expression='biso_Ba = biso_La')
+    project.analysis.constraints.create(expression='occ_Ba = 1 - occ_La')
+
+    # More fit patams
+    project.structures['lbco'].atom_sites['La'].occupancy.free = True
+
+    # Save to a directory
+    project.save_as('lbco_project')
+
+    # Load Project from Directory
+    project = ed.Project.load('lbco_project')
+
+    # Perform Analysis
+    project.analysis.fit()
+
+    # Compare fit quality
+    assert_almost_equal(
+        project.analysis.fit_results.reduced_chi_square,
+        desired=1.28,
+        decimal=1,
+    )
+
+
 if __name__ == '__main__':
     test_fit_neutron_pd_cwl_hs()
     test_single_fit_neutron_pd_cwl_lbco()
