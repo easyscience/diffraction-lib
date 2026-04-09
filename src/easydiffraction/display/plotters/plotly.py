2615689764d9f40e6195a546631b1112b5e320d7
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-04-09 08:56:42 +0200

    Refactor example notebooks to improve clarity

--- src/easydiffraction/display/plotters/plotly.py
+++ src/easydiffraction/display/plotters/plotly.py
@@ -155,8 +155,7 @@ class PlotlyPlotter(PlotterBase):
                 'yanchor': 'middle',
             },
             hoverongaps=False,
-            hovertemplate=f'x: %{{x}}<br>y: %{{y}}<br>correlation: %{{z:'
-            f'.{precision}f}}<extra></extra>',
+            hovertemplate=f'x: %{{x}}<br>y: %{{y}}<br>corr: %{{z:.{precision}f}}<extra></extra>',
         )
         label_trace = self._get_correlation_label_trace(
             corr_df,
