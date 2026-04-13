d153bcf9064a81dc9ff23917acd4919480a5b902
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-04-14 00:47:15 +0200

    Bump dependencies

--- tests/integration/scipp-analysis/dream/test_analyze_reduced_data.py
+++ tests/integration/scipp-analysis/dream/test_analyze_reduced_data.py
@@ -57,14 +57,7 @@ def project_with_data(
     prepared_cif_path: str,
 ) -> ed.Project:
     """Create project with structure, experiment data, and
-    configuration.
-
-    1. Define project
-    2. Add structure manually defined
-    3. Modify experiment CIF file
-    4. Add experiment from modified CIF file
-    5. Modify default experiment configuration
-    """
+    configuration."""
     # Step 1: Define Project
     project = ed.Project()
 
@@ -133,11 +126,7 @@ def project_with_data(
 def fitted_project(
     project_with_data: ed.Project,
 ) -> ed.Project:
-    """Perform fit and return project with results.
-
-    6. Select parameters to be fitted
-    7. Do fitting
-    """
+    """Perform fit and return project with results."""
     project = project_with_data
     structure = project.structures['diamond']
     experiment = project.experiments['reduced_tof']
@@ -154,7 +143,6 @@ def fitted_project(
     experiment.peak.broad_gauss_sigma_1.free = True
     experiment.peak.exp_decay_beta_0.free = True
 
-    # Set free parameters for background
     for point in experiment.background:
         point.y.free = True
 
