8796f27b9cb0c85b5097c21784d1f9aa916d7f3c
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-05-16 10:49:07 +0200

    Add fitting category replacing fit configuration surface

--- src/easydiffraction/analysis/__init__.py
+++ src/easydiffraction/analysis/__init__.py
@@ -1,2 +1,5 @@
 # SPDX-FileCopyrightText: 2025 EasyScience contributors <https://github.com/easyscience>
 # SPDX-License-Identifier: BSD-3-Clause
+
+from easydiffraction.analysis.categories.fitting import Fitting
+from easydiffraction.analysis.categories.fitting import FittingFactory
---
+++ src/easydiffraction/analysis/categories/fitting/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2026 EasyScience contributors <https://github.com/easyscience>
+# SPDX-License-Identifier: BSD-3-Clause
+
+from easydiffraction.analysis.categories.fitting.default import Fitting
+from easydiffraction.analysis.categories.fitting.factory import FittingFactory
