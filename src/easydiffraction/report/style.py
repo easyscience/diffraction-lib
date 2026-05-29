88b591b61cdee50489e6f87948fba22a2417f092
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-05-30 00:50:37 +0200

    Style PDF report links to match HTML

--- src/easydiffraction/report/style.py
+++ src/easydiffraction/report/style.py
@@ -8,6 +8,7 @@ REPORT_AXIS_RGB = (190, 199, 208)
 REPORT_TABLE_INNER_RGB = (217, 223, 228)
 REPORT_CHART_GRID_RGB = (235, 240, 248)
 REPORT_ROW_RGB = (235, 240, 248)
+REPORT_LINK_RGB = (36, 90, 155)  # mirrors --link (#245a9b) in html/style.css
 REPORT_SUBTITLE = 'EasyDiffraction Report'
 REPORT_HTML_FONT_FAMILY = '"PT Sans", "Trebuchet MS", "Segoe UI", sans-serif'
 REPORT_TEX_FONT_FAMILY = 'Nunito'
@@ -25,6 +26,8 @@ def report_style_context() -> dict[str, object]:
         'chart_grid_rgb': _rgb_channels(REPORT_CHART_GRID_RGB),
         'row_hex': _rgb_hex(REPORT_ROW_RGB),
         'row_rgb': _rgb_channels(REPORT_ROW_RGB),
+        'link_hex': _rgb_hex(REPORT_LINK_RGB),
+        'link_rgb': _rgb_channels(REPORT_LINK_RGB),
         'html_font_family': REPORT_HTML_FONT_FAMILY,
         'tex_font_family': REPORT_TEX_FONT_FAMILY,
         'tex_math_font_family': REPORT_TEX_MATH_FONT_FAMILY,
