742be184ce05531d2b578ced15f64e7b5fa6f177
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-05-29 19:56:21 +0200

    Update report integration save test

--- tests/integration/fitting/test_summary_report.py
+++ tests/integration/fitting/test_summary_report.py
@@ -32,6 +32,10 @@ def test_show_fitting_details(lbco_fitted_project):
 def test_report_save(lbco_fitted_project, tmp_path):
     project = lbco_fitted_project
     project.save_as(str(tmp_path / 'proj'))
-    report_path = project.report.save()
+    project.report.cif = True
+    report_paths = project.report.save()
+    report_path = report_paths[0]
+
+    assert len(report_paths) == 1
     assert report_path.is_file()
     assert report_path.read_text(encoding='utf-8').startswith('data_global\n')
