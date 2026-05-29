45f5aa435ddc0106dfafc803c64f5889b4c2e1d5
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-05-29 19:23:18 +0200

    Restore lazy report import

--- src/easydiffraction/report/__init__.py
+++ src/easydiffraction/report/__init__.py
@@ -4,4 +4,12 @@
 
 from __future__ import annotations
 
-from easydiffraction.project.categories.report.default import Report
+
+def __getattr__(name: str) -> object:
+    """Load report objects that would otherwise form import cycles."""
+    if name == 'Report':
+        from easydiffraction.project.categories.report.default import Report  # noqa: PLC0415
+
+        return Report
+    msg = f'module {__name__!r} has no attribute {name!r}'
+    raise AttributeError(msg)
