2b4e6251821b90ba5807a471d42a4b7852172751
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-09-14 12:12:26 +0200

    Normalize imported CIF datablock names

--- tests/unit/easydiffraction/datablocks/structure/item/test_factory.py
+++ tests/unit/easydiffraction/datablocks/structure/item/test_factory.py
@@ -7,3 +7,10 @@ from easydiffraction.datablocks.structure.item.factory import StructureFactory
 def test_from_scratch():
     m = StructureFactory.from_scratch(name='abc')
     assert m.name == 'abc'
+
+
+def test_from_cif_str_normalizes_datablock_name():
+    structure = StructureFactory.from_cif_str('data_83267-ICSD\n')
+
+    assert structure.name == '83267-icsd'
+    assert structure.as_cif.startswith('data_83267-icsd\n')
